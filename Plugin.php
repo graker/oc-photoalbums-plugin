000097fd217cd1caae5e2f25047b007958651fb4
graker <graker@gmail.com>
2016-02-07 16:19:14 +0300

    Initial plugin version

---
+++ Plugin.php
@@ -0,0 +1,57 @@
+<?php namespace Graker\PhotoAlbums;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * PhotoAlbums Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'PhotoAlbums',
+            'description' => 'No description provided yet...',
+            'author'      => 'Graker',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return [];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return [];
+    }
+
+}
