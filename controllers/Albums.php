f4987d0c38ba9dd342336a3fe8902893f1f74b48
graker <graker@gmail.com>
2016-02-08 14:12:28 +0300

    Added photos list with add button into album create/update forms

--- controllers/Albums.php
+++ controllers/Albums.php
@@ -8,18 +8,20 @@ use Backend\Classes\Controller;
  */
 class Albums extends Controller
 {
-    public $implement = [
-        'Backend.Behaviors.FormController',
-        'Backend.Behaviors.ListController'
-    ];
+  public $implement = [
+    'Backend.Behaviors.FormController',
+    'Backend.Behaviors.ListController',
+    'Backend.Behaviors.RelationController',
+  ];
 
-    public $formConfig = 'config_form.yaml';
-    public $listConfig = 'config_list.yaml';
+  public $formConfig = 'config_form.yaml';
+  public $listConfig = 'config_list.yaml';
+  public $relationConfig = 'config_relation.yaml';
 
-    public function __construct()
-    {
-        parent::__construct();
+  public function __construct()
+  {
+    parent::__construct();
 
-        BackendMenu::setContext('Graker.PhotoAlbums', 'photoalbums', 'albums');
-    }
+    BackendMenu::setContext('Graker.PhotoAlbums', 'photoalbums', 'albums');
+  }
 }
